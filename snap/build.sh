f75866346e33ef9a7d3a0e21cdc084a23073c628
Claudio André <claudioandre.br@gmail.com>
2023-04-16 12:47:24 -0300

    snap: migrate snap package to core22

--- snap/build.sh
+++ snap/build.sh
@@ -25,22 +25,37 @@ X86_NO_OPENMP="--disable-native-tests $SYSTEM_WIDE --disable-openmp"
 OTHER_REGULAR="$SYSTEM_WIDE"
 OTHER_NO_OPENMP="$SYSTEM_WIDE --disable-openmp"
 
-# Get JtR source code and adjust it to create a SNAP package
-git clone --depth 10 https://github.com/openwall/john.git tmp
-cp -r tmp/. .
+if [[ "$1" == "PULL" ]]; then
+    # Get JtR source code and adjust it to create a SNAP package
+    rm -rf tmp
+    git clone --depth 10 https://github.com/openwall/john.git tmp
+    cp -r tmp/. .
+
+    # Uncomment for a release
+    #_JUMBO_RELEASE="15b3b7c25fc8ac34f2504d53f0c94bbf4ec12596"
+
+    # Make it a reproducible build
+    if [[ -n "$_JUMBO_RELEASE" ]]; then
+        echo "Deploying the release $_JUMBO_RELEASE"
+        git pull --unshallow
+        git checkout "$_JUMBO_RELEASE"
+    fi
+
+    # Set package version
+    git rev-parse --short HEAD 2>/dev/null > ../../../My_VERSION.TXT
+    cp ../../../My_VERSION.TXT My_VERSION.TXT
+    cp ../../../My_VERSION.TXT src/My_VERSION.TXT
+
+    # Get the script that computes the package version
+    wget https://raw.githubusercontent.com/claudioandre-br/JtR-CI/master/tests/package_version.sh
+    chmod +x package_version.sh
+    cp package_version.sh ../../../package_version.sh
+
+    exit 0
+fi
 
 # We are in packages folder, change to JtR folder
 cd src
-
-# Uncomment for a release
-#_JUMBO_RELEASE="15b3b7c25fc8ac34f2504d53f0c94bbf4ec12596"
-
-# Make it a reproducible build
-if [[ -n "$_JUMBO_RELEASE" ]]; then
-    echo "Deploying the release $_JUMBO_RELEASE"
-    git pull --unshallow
-    git checkout "$_JUMBO_RELEASE"
-fi
 
 wget https://raw.githubusercontent.com/openwall/john-packages/master/patches/0001-Handle-self-confined-system-wide-build.patch
 patch < 0001-Handle-self-confined-system-wide-build.patch
@@ -48,12 +63,9 @@ patch < 0001-Handle-self-confined-system-wide-build.patch
 wget https://raw.githubusercontent.com/claudioandre-br/JohnTheRipper/bleeding-jumbo/be.patch; _BE_TEST=$?
 
 if [[ "$_BE_TEST" == 0 ]]; then
-    echo "Applying a patch be.patch from claudioandre-br/JohnTheRipper/bleeding-jumbo"
+    echo "Applying a testing purpose patch called 'be.patch' from claudioandre-br/JohnTheRipper"
     git apply be.patch
 fi
-
-# Set package version
-git rev-parse --short HEAD 2>/dev/null > ../../../../My_VERSION.TXT
 
 # Force CFLAGS with -O2
 export CFLAGS="-O2 $CFLAGS"
@@ -70,27 +82,24 @@ echo ""
 echo "---------------------------- BUILDING -----------------------------"
 
 if [[ "$arch" == "x86_64" ]]; then
-    # Workaround (WTF)
-    sudo ln -s /usr/lib/x86_64-linux-gnu/libOpenCL.so.1 /usr/lib/x86_64-linux-gnu/libOpenCL.so
-
     # x86_64 CPU (OMP and SIMD fallback)
     ./configure $X86_NO_OPENMP --enable-simd=sse2   CPPFLAGS="-D_SNAP -D_BOXED" && do_build ../run/john-sse2
-    ./configure $X86_REGULAR   --enable-simd=sse2   CPPFLAGS="-D_SNAP -D_BOXED -DOMP_FALLBACK -DOMP_FALLBACK_BINARY=\"\\\"john-sse2\\\"\"" && do_build ../run/john-sse2-omp
+    ./configure $X86_REGULAR   --enable-simd=sse2   CPPFLAGS="-D_SNAP -D_BOXED -DOMP_FALLBACK_BINARY=\"\\\"john-sse2\\\"\"" && do_build ../run/john-sse2-omp
     ./configure $X86_NO_OPENMP --enable-simd=avx    CPPFLAGS="-D_SNAP -D_BOXED" && do_build ../run/john-avx
-    ./configure $X86_REGULAR   --enable-simd=avx    CPPFLAGS="-D_SNAP -D_BOXED -DOMP_FALLBACK -DOMP_FALLBACK_BINARY=\"\\\"john-avx\\\"\" -DCPU_FALLBACK -DCPU_FALLBACK_BINARY=\"\\\"john-sse2-omp\\\"\"" && do_build ../run/john-avx-omp
+    ./configure $X86_REGULAR   --enable-simd=avx    CPPFLAGS="-D_SNAP -D_BOXED -DOMP_FALLBACK_BINARY=\"\\\"john-avx\\\"\" -DCPU_FALLBACK_BINARY=\"\\\"john-sse2-omp\\\"\"" && do_build ../run/john-avx-omp
     ./configure $X86_NO_OPENMP --enable-simd=avx2   CPPFLAGS="-D_SNAP -D_BOXED" && do_build ../run/john-avx2
-    ./configure $X86_REGULAR   --enable-simd=avx2   CPPFLAGS="-D_SNAP -D_BOXED -DOMP_FALLBACK -DOMP_FALLBACK_BINARY=\"\\\"john-avx2\\\"\" -DCPU_FALLBACK -DCPU_FALLBACK_BINARY=\"\\\"john-avx-omp\\\"\"" && do_build ../run/john-avx2-omp
+    ./configure $X86_REGULAR   --enable-simd=avx2   CPPFLAGS="-D_SNAP -D_BOXED -DOMP_FALLBACK_BINARY=\"\\\"john-avx2\\\"\" -DCPU_FALLBACK_BINARY=\"\\\"john-avx-omp\\\"\"" && do_build ../run/john-avx2-omp
     ./configure $X86_NO_OPENMP --enable-simd=avx512f  CPPFLAGS="-D_SNAP -D_BOXED" && do_build ../run/john-avx512f
-    ./configure $X86_REGULAR   --enable-simd=avx512f  CPPFLAGS="-D_SNAP -D_BOXED -DOMP_FALLBACK -DOMP_FALLBACK_BINARY=\"\\\"john-avx512f\\\"\" -DCPU_FALLBACK -DCPU_FALLBACK_BINARY=\"\\\"john-avx2-omp\\\"\"" && do_build ../run/john-avx512f-omp
+    ./configure $X86_REGULAR   --enable-simd=avx512f  CPPFLAGS="-D_SNAP -D_BOXED -DOMP_FALLBACK_BINARY=\"\\\"john-avx512f\\\"\" -DCPU_FALLBACK_BINARY=\"\\\"john-avx2-omp\\\"\"" && do_build ../run/john-avx512f-omp
     ./configure $X86_NO_OPENMP --enable-simd=avx512bw CPPFLAGS="-D_SNAP -D_BOXED" && do_build ../run/john-avx512bw
-    ./configure $X86_REGULAR   --enable-simd=avx512bw CPPFLAGS="-D_SNAP -D_BOXED -DOMP_FALLBACK -DOMP_FALLBACK_BINARY=\"\\\"john-avx512bw\\\"\" -DCPU_FALLBACK -DCPU_FALLBACK_BINARY=\"\\\"john-avx512f-omp\\\"\"" && do_build ../run/john-avx512bw-omp
+    ./configure $X86_REGULAR   --enable-simd=avx512bw CPPFLAGS="-D_SNAP -D_BOXED -DOMP_FALLBACK_BINARY=\"\\\"john-avx512bw\\\"\" -DCPU_FALLBACK_BINARY=\"\\\"john-avx512f-omp\\\"\"" && do_build ../run/john-avx512bw-omp
 
     #Create a 'john' executable
     ln -s ../run/john-avx512bw-omp ../run/john
 else
     # Non X86 CPU (OMP fallback)
     ./configure $OTHER_NO_OPENMP   CPPFLAGS="-D_SNAP -D_BOXED" && do_build "../run/john-$arch"
-    ./configure $OTHER_REGULAR     CPPFLAGS="-D_SNAP -D_BOXED -DOMP_FALLBACK -DOMP_FALLBACK_BINARY=\"\\\"john-$arch\\\"\"" && do_build ../run/john-omp
+    ./configure $OTHER_REGULAR     CPPFLAGS="-D_SNAP -D_BOXED -DOMP_FALLBACK_BINARY=\"\\\"john-$arch\\\"\"" && do_build ../run/john-omp
 
     #Create a 'john' executable
     ln -s ../run/john-omp ../run/john
@@ -103,6 +112,7 @@ echo "OpenMP=No" >> ../run/Defaults
 echo "OpenCL=Yes # ONLY on x86_64, otherwise No" >> ../run/Defaults
 echo "Optional Libraries=Yes" >> ../run/Defaults
 echo "Regex, OpenMPI, Experimental Code, ZTEX=No" >> ../run/Defaults
+echo "Version=$(../package_version.sh)" >> ../run/Defaults
 
 # To be able to run testing
 sudo apt-get install -y language-pack-en
@@ -121,8 +131,3 @@ source run_tests.sh
 
 wget https://raw.githubusercontent.com/claudioandre-br/JtR-CI/master/tests/clean_package.sh
 source clean_package.sh
-
-# Get the script that computes the package version
-wget https://raw.githubusercontent.com/claudioandre-br/JtR-CI/master/tests/package_version.sh
-chmod +x package_version.sh
-cp package_version.sh ../../../../package_version.sh
