630dce6a5b06fea90995829257f5d262bad39345
Claudio André <claudioandre.br@gmail.com>
2023-05-05 10:47:01 -0300

    docs: adjust files copyright header

--- cloud-tool/build-john.sh
+++ cloud-tool/build-john.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
-######################################################################
+###############################################################################
+#        _       _             _   _            _____  _
+#       | |     | |           | | | |          |  __ \(_)
+#       | | ___ | |__  _ __   | |_| |__   ___  | |__) |_ _ __  _ __   ___ _ __
+#   _   | |/ _ \| '_ \| '_ \  | __| '_ \ / _ \ |  _  /| | '_ \| '_ \ / _ \ '__|
+#  | |__| | (_) | | | | | | | | |_| | | |  __/ | | \ \| | |_) | |_) |  __/ |
+#   \____/ \___/|_| |_|_| |_|  \__|_| |_|\___| |_|  \_\_| .__/| .__/ \___|_|
+#                                                       | |   | |
+#                                                       |_|   |_|
+#
 # Copyright (c) 2021 Claudio André <claudioandre.br at gmail.com>
 #
 # This program comes with ABSOLUTELY NO WARRANTY; express or implied.
@@ -8,7 +17,9 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, as expressed in version 2, seen at
 # http://www.gnu.org/licenses/gpl-2.0.html
-######################################################################
+###############################################################################
+# Script to automate the build of john the ripper
+# More info at https://github.com/openwall/john-packages
 
 # Prepare the environment
 sudo apt-get -y install git build-essential libssl-dev zlib1g-dev libgmp-dev libpcap-dev libbz2-dev
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-######################################################################
+###############################################################################
+#        _       _             _   _            _____  _
+#       | |     | |           | | | |          |  __ \(_)
+#       | | ___ | |__  _ __   | |_| |__   ___  | |__) |_ _ __  _ __   ___ _ __
+#   _   | |/ _ \| '_ \| '_ \  | __| '_ \ / _ \ |  _  /| | '_ \| '_ \ / _ \ '__|
+#  | |__| | (_) | | | | | | | | |_| | | |  __/ | | \ \| | |_) | |_) |  __/ |
+#   \____/ \___/|_| |_|_| |_|  \__|_| |_|\___| |_|  \_\_| .__/| .__/ \___|_|
+#                                                       | |   | |
+#                                                       |_|   |_|
+#
 # Copyright (c) 2019-2023 Claudio André <claudioandre.br at gmail.com>
 #
 # This program comes with ABSOLUTELY NO WARRANTY; express or implied.
@@ -8,7 +17,9 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, as expressed in version 2, seen at
 # http://www.gnu.org/licenses/gpl-2.0.html
-######################################################################
+###############################################################################
+# Script that serves as the entrypoint for the Docker image of john the ripper
+# More info at https://github.com/openwall/john-packages
 
 set -e
 echo "$@"
