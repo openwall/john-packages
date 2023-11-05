5ca4ebbd5c21fb86a6b4971540c756e6b0891382
Claudio André <claudioandre.br@gmail.com>
2023-11-05 10:55:08 -0300

    flatpak: stop building SSE2 binaries
    Signed-off-by: Claudio André <claudioandre.br@gmail.com>

--- deploy/flatpak/build.sh
+++ deploy/flatpak/build.sh
@@ -88,10 +88,8 @@ if [[ -z "$TASK" ]]; then
 
     if [[ "$arch" == "x86_64" ]]; then
         # x86_64 CPU (OMP and SIMD fallback)
-        do_configure "$X86_NO_OPENMP" --enable-simd=sse2   CPPFLAGS="-D_BOXED" && do_build ../run/john-sse2
-        do_configure "$X86_REGULAR"   --enable-simd=sse2   CPPFLAGS="-D_BOXED -DOMP_FALLBACK_BINARY=\"\\\"john-sse2\\\"\"" && do_build ../run/john-sse2-omp
         do_configure "$X86_NO_OPENMP" --enable-simd=avx    CPPFLAGS="-D_BOXED" && do_build ../run/john-avx
-        do_configure "$X86_REGULAR"   --enable-simd=avx    CPPFLAGS="-D_BOXED -DOMP_FALLBACK_BINARY=\"\\\"john-avx\\\"\" -DCPU_FALLBACK_BINARY=\"\\\"john-sse2-omp\\\"\"" && do_build ../run/john-avx-omp
+        do_configure "$X86_REGULAR"   --enable-simd=avx    CPPFLAGS="-D_BOXED -DOMP_FALLBACK_BINARY=\"\\\"john-avx\\\"\" " && do_build ../run/john-avx-omp
         do_configure "$X86_NO_OPENMP" --enable-simd=avx2   CPPFLAGS="-D_BOXED" && do_build ../run/john-avx2
         do_configure "$X86_REGULAR"   --enable-simd=avx2   CPPFLAGS="-D_BOXED -DOMP_FALLBACK_BINARY=\"\\\"john-avx2\\\"\" -DCPU_FALLBACK_BINARY=\"\\\"john-avx-omp\\\"\"" && do_build ../run/john-avx2-omp
         do_configure "$X86_NO_OPENMP" --enable-simd=avx512f  CPPFLAGS="-D_BOXED" && do_build ../run/john-avx512f
