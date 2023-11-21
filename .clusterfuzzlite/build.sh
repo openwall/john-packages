6a7719e0e9c60e72d6db763ae2a6aa090d54cf2b
Claudio André <claudioandre.br@gmail.com>
2023-11-21 14:15:32 -0300

    CI: add a fuzzing test
    Basically to show static analyzers we are doing our homework.
    
    It only contains part of the task that is required to do the complete
    fuzz. Several details need to be done upstream and I'm not going to do
    that now.
    
    Signed-off-by: Claudio André <claudioandre.br@gmail.com>

---
+++ .clusterfuzzlite/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+###############################################################################
+#        _       _             _   _            _____  _
+#       | |     | |           | | | |          |  __ \(_)
+#       | | ___ | |__  _ __   | |_| |__   ___  | |__) |_ _ __  _ __   ___ _ __
+#   _   | |/ _ \| '_ \| '_ \  | __| '_ \ / _ \ |  _  /| | '_ \| '_ \ / _ \ '__|
+#  | |__| | (_) | | | | | | | | |_| | | |  __/ | | \ \| | |_) | |_) |  __/ |
+#   \____/ \___/|_| |_|_| |_|  \__|_| |_|\___| |_|  \_\_| .__/| .__/ \___|_|
+#                                                       | |   | |
+#                                                       |_|   |_|
+#
+# Copyright (c) 2023 Claudio André <claudioandre.br at gmail.com>
+#
+# This program comes with ABSOLUTELY NO WARRANTY; express or implied.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, as expressed in version 2, seen at
+# http://www.gnu.org/licenses/gpl-2.0.html
+###############################################################################
+# Script for building John the Ripper (for fuzzing)
+# More info at https://github.com/openwall/john-packages
+
+cd /upstream/src || exit 1
+
+./configure --enable-asan --enable-ubsan --enable-fuzz
+make -sj4
+
+cp ../run/john "$OUT"/
+
+../run/john --test=0
