55a7f94d03e9842c5548a29c007d2f818de264b4
Claudio André <dev@claudioandre.slmail.me>
2024-04-02 23:28:12 -0300

    release: add the updated CI build logs
    Use your own commit to make the release commit leaner.
    
    Signed-off-by: Claudio André <dev@claudioandre.slmail.me>

--- Releases/rolling/get-files.sh
+++ Releases/rolling/get-files.sh
@@ -22,11 +22,11 @@
 # More info at https://github.com/openwall/john-packages
 
 # Directory names and folders
-FLATPAK="5132721237"
-AZURE_ID="447"
+FLATPAK="6533120921"
+AZURE_ID="482"
 
 # Azure Windows package (64 bits) ##############################################
-wget "https://dev.azure.com/claudioandre-br/40224313-b91e-465d-852b-fc4ea516f33e/_apis/build/builds/$AZURE_ID/logs/123" \
+wget "https://dev.azure.com/claudioandre-br/40224313-b91e-465d-852b-fc4ea516f33e/_apis/build/builds/$AZURE_ID/logs/128" \
     -O windows_log.txt
 
 # GitLab (Linux Flatpak app) ###################################################
@@ -34,19 +34,12 @@ wget "https://gitlab.com/claudioandre-br/JtR-CI/-/jobs/$FLATPAK/raw" \
     -O bundle_log.txt
 
 # macOS ########################################################################
-wget "https://api.cirrus-ci.com/v1/task/4704739347136512/logs/build.log" \
+wget "https://api.cirrus-ci.com/v1/task/4652265359802368/logs/build.log" \
     -O macM1_log.txt
-wget "https://api.cirrus-ci.com/v1/task/4704739347136512/logs/package.log" \
+wget "https://api.cirrus-ci.com/v1/task/4652265359802368/logs/package.log" \
     -O ->> macM1_log.txt
-
-wget "https://circleci.com/api/v1.1/project/github/claudioandre-br/JohnTheRipper/6318/output/102/0?file=true&allocation-id=650b9460965983317c043b4b-0-build%2FABCDEFGH" \
-    -O macX86_log.txt
 
 # Snap App #####################################################################
 rm buildlog*.gz
 
-wget https://launchpad.net/~claudioandre.br/john/+snap/john-the-ripper/+build/2236339/+files/buildlog_snap_ubuntu_jammy_armhf_john-the-ripper_BUILDING.txt.gz
-wget https://launchpad.net/~claudioandre.br/john/+snap/john-the-ripper/+build/2236341/+files/buildlog_snap_ubuntu_jammy_s390x_john-the-ripper_BUILDING.txt.gz
-wget https://launchpad.net/~claudioandre.br/john/+snap/john-the-ripper/+build/2236340/+files/buildlog_snap_ubuntu_jammy_arm64_john-the-ripper_BUILDING.txt.gz
-wget https://launchpad.net/~claudioandre.br/john/+snap/john-the-ripper/+build/2236338/+files/buildlog_snap_ubuntu_jammy_amd64_john-the-ripper_BUILDING.txt.gz
-wget https://launchpad.net/~claudioandre.br/john/+snap/john-the-ripper/+build/2236315/+files/buildlog_snap_ubuntu_jammy_ppc64el_john-the-ripper_BUILDING.txt.gz
+wget https://launchpad.net/~claudioandre.br/john/+snap/john-the-ripper/+build/2438416/+files/buildlog_snap_ubuntu_jammy_amd64_john-the-ripper_BUILDING.txt.gz
