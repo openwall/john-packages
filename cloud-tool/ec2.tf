58e90877e99a5c059a65cff8fe3504857a98d2b2
Claudio André <dev@claudioandre.slmail.me>
2024-11-09 12:25:23 -0300

    security(ec2.tf): turn on encryption (#577)
    Improve the cloud tool's default security settings.
    
    Block devices should be encrypted to ensure sensitive data is held
    securely at rest.
    
    Signed-off-by: Claudio André <dev@claudioandre.slmail.me>

--- cloud-tool/ec2.tf
+++ cloud-tool/ec2.tf
@@ -48,6 +48,10 @@ resource "aws_instance" "worker" {
     cpu_credits = "standard"
   }
 
+  root_block_device {
+    encrypted = true
+  }
+
   metadata_options {
     http_tokens                 = "required"
     http_put_response_hop_limit = 2
