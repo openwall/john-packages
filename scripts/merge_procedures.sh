b9d39fd3bac2c367a4a1547e855190358fa9e92e
Claudio André <dev@claudioandre.slmail.me>
2024-11-17 11:16:37 -0300

    fix(merge-bot): adjust the API in use (#618)
    Do NOT use gh pr view #NUM --json latestReviews
    Use instead gh pr view #NUM --json reviewDecision
    
    Signed-off-by: Claudio André <dev@claudioandre.slmail.me>

--- scripts/merge_procedures.sh
+++ scripts/merge_procedures.sh
@@ -41,15 +41,13 @@ if [[ "$REQUEST" == "bot: MERGE trial" ]]; then
 	MY_MESSAGE+="No changes will be submitted to GitHub."
 	TRIAL="true"
 fi
-gh pr checkout "$PR_URL"
-
-REVIEWS_STATUS="$(gh pr status --json latestReviews --jq '.currentBranch.latestReviews[].state == "APPROVED"')"
+REVIEWS_STATUS="$(gh pr view "$PR_URL" --json reviewDecision --jq '.reviewDecision == "APPROVED"')"
 APPROVALS="$(echo "$REVIEWS_STATUS" | grep -c 'true' || true)"
-MERGE_STATUS="$(gh pr status --json mergeStateStatus --jq '.currentBranch.mergeStateStatus == "CLEAN"')"
+MERGE_STATUS="$(gh pr view "$PR_URL" --json mergeStateStatus --jq '.mergeStateStatus == "CLEAN"')"
 STATUS="$(echo "$MERGE_STATUS" | grep -c 'true' || true)"
 
 echo "**********************************************************************"
-echo -e "Approvals: $APPROVALS"
+echo -e "Approved: $REVIEWS_STATUS"
 echo -e "Mergeable: $MERGE_STATUS"
 echo -e "$MY_MESSAGE"
 echo -e "---------------"
@@ -57,15 +55,15 @@ echo -e "Reviews: $REVIEWS_STATUS"
 echo "**********************************************************************"
 
 if [[ "$TRIAL" == 'true' ]]; then
-	echo "latestReviews: $(gh pr status --json latestReviews)"
-	echo "mergeStateStatus: $(gh pr status --json mergeStateStatus)"
+	echo "reviewDecision: $(gh pr view "$PR_URL" --json reviewDecision)"
+	echo "mergeStateStatus: $(gh pr view "$PR_URL" --json mergeStateStatus)"
 	echo "**********************************************************************"
 fi
 git config --global user.name "Continuous Integration"
 git config --global user.email "username@users.noreply.github.com"
 DEST_BRANCH="$BRANCH"
 
-if [[ ("$APPROVALS" -ge 1 && "$STATUS" -eq 1) || "$SKIP" == 'true' ]]; then
+if [[ ("$APPROVALS" -ge 1 && "$STATUS" -ge 1) || "$SKIP" == 'true' ]]; then
 	if [[ "$OWNER" != "openwall" ]]; then
 		echo "The PR comes from a fork."
 		DEST_BRANCH="$OWNER-$BRANCH"
