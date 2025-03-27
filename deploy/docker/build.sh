c694033fbc4652f74c52bc33cc3d89613ffc1923
Claudio André <dev@claudioandre.slmail.me>
2025-03-27 09:22:11 -0300

    build: update how we build AVX512* binaries (#778)
    The way we should call `./configure` has changed.
    
    Signed-off-by: Claudio André <dev@claudioandre.slmail.me>

--- deploy/docker/build.sh
+++ deploy/docker/build.sh
@@ -89,8 +89,8 @@ if [ "$arch" == "x86_64" ]; then
 	do_configure "$X86_REGULAR" --enable-simd=avx && do_build ../run/john-avx-omp
 	do_configure "$X86_NO_OPENMP" --enable-simd=avx2 && do_build ../run/john-avx2
 	do_configure "$X86_REGULAR" --enable-simd=avx2 && do_build ../run/john-avx2-omp
-	do_configure "$X86_NO_OPENMP" --enable-simd=avx512bw && do_build ../run/john-avx512bw
-	do_configure "$X86_REGULAR" --enable-simd=avx512bw && do_build ../run/john-avx512bw-omp
+	do_configure "$X86_NO_OPENMP" --enable-simd=avx512 && do_build ../run/john-avx512bw
+	do_configure "$X86_REGULAR" --enable-simd=avx512 && do_build ../run/john-avx512bw-omp
 	BINARY="john-avx-omp"
 else
 	# Non X86 CPU (OMP fallback)
